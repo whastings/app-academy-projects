832e18b494ab6b98bb3ed0f878d00967c0144947
Will Hastings <will@objectdotcreate.net>
2014-02-02 12:03:04 -0800

    Word Chain solo: Add method to find all words that can be reached from a given word.

--- ruby/word_chains_solo.rb
+++ ruby/word_chains_solo.rb
@@ -1,5 +1,7 @@
 # Redo of word chains solo.
 
+require "set"
+
 def adjacent_words(word, dictionary)
   words = []
   (0...word.length).each do |char_index|
@@ -11,4 +13,26 @@ def adjacent_words(word, dictionary)
     end
   end
   words
+end
+
+def words_of_length(word, dictionary)
+  candidate_words = Set.new
+  dictionary.each do |dictionary_word|
+    candidate_words << dictionary_word if word.length == dictionary_word.length
+  end
+  candidate_words
+end
+
+def explore_words(source, dictionary)
+  words_to_expand = [source]
+  all_reachable_words = Set.new([source])
+  candidate_words = words_of_length(source, dictionary)
+  until words_to_expand.empty?
+    word = words_to_expand.shift
+    similar_words = adjacent_words(word, candidate_words)
+    candidate_words.subtract(similar_words)
+    words_to_expand.concat(similar_words)
+    all_reachable_words.merge(similar_words)
+  end
+  all_reachable_words
 end
