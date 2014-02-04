b1e357167aed0706a12c7cfcf49956c222513b67
Will Hastings <will@objectdotcreate.net>
2014-02-04 11:38:08 -0800

    add bishop, rook and queen moves.

--- chess.rb
+++ chess.rb
@@ -24,25 +24,44 @@ class SteppingPiece < Piece
 
 end
 
-# class SlidingPiece < Piece
-#   ORTHAGONAL = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-#   DIAGONAL = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
-#
-#
-# end
+class SlidingPiece < Piece
+  ORTHAGONAL = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+  DIAGONAL = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+
+  def moves
+    x, y = @position
+    possible_moves = []
+    (1..7).each do |multiplier|
+      move_dirs.each do |x_offset, y_offset|
+        new_x, new_y = x + (multiplier * x_offset), y + (multiplier * y_offset)
+        if ((0..7).cover?(new_x) && (0..7).cover?(new_y))
+          possible_moves << [new_x, new_y]
+        end
+      end
+    end
+    possible_moves
+  end
+
+end
 
 
-# class Bishop < SlidingPiece
-#
-# end
-#
-# class Rook < SlidingPiece
-#
-# end
-#
-# class Queen < SlidingPiece
-#
-# end
+class Bishop < SlidingPiece
+  def move_dirs
+    DIAGONAL
+  end
+end
+
+class Rook < SlidingPiece
+  def move_dirs
+    ORTHAGONAL
+  end
+end
+
+class Queen < SlidingPiece
+  def move_dirs
+    DIAGONAL + ORTHAGONAL
+  end
+end
 
 class Knight < SteppingPiece
 
--- chess_spec.rb
+++ chess_spec.rb
@@ -5,3 +5,12 @@ puts "Knight moves: #{k.moves}"
 
 king = King.new([0, 0], nil, nil)
 puts "King moves: #{king.moves}"
+
+b = Bishop.new([0, 0], nil, nil)
+puts "Bishop moves: #{b.moves}"
+
+r = Rook.new([0, 0], nil, nil)
+puts "Rook moves: #{r.moves}"
+
+q = Queen.new([0, 0], nil, nil)
+puts "Queen moves: #{q.moves}"
