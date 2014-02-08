1ede2210429f6950a3f0322327c39ca7343ea936
Will Hastings <will@objectdotcreate.net>
2014-02-08 13:45:12 -0800

    Enable Poker game to deal cards, take bets, and play draws.

--- lib/poker.rb
+++ lib/poker.rb
@@ -1,2 +1,41 @@
-require './deck'
-require './player'
+require 'deck'
+require 'player'
+
+class Poker
+  attr_accessor :pot, :players
+
+  def initialize(players)
+    @players = players
+    @deck = Deck.new
+    @deck.shuffle
+    @pot = 0
+  end
+
+  def deal_cards
+    @players.each do |player|
+      player.add_cards(@deck.draw(5))
+    end
+  end
+
+  def take_bets
+    highest_bet, players_to_remove = 0, []
+    @players.each do |player|
+      player_bet = player.place_bet(highest_bet)
+      if player_bet == -1
+        players_to_remove << player
+        next
+      end
+      @pot += player_bet
+      highest_bet = player_bet if player_bet > highest_bet
+    end
+    @players.delete_if { |player| players_to_remove.include?(player) }
+  end
+
+  def play_draw
+    @players.each do |player|
+      num_cards = player.discard_cards
+      player.add_cards(@deck.draw(num_cards)) if num_cards > 0
+    end
+  end
+
+end
---
+++ spec/poker_spec.rb
@@ -0,0 +1,62 @@
+require 'rspec'
+require 'poker'
+
+describe Poker do
+  let!(:players) do
+    [double("Weyman", pot: 20, discard_cards: 2),
+     double("Will", pot: 5, discard_cards: 3)]
+  end
+  subject(:poker) { Poker.new(players) }
+
+  describe "#deal_cards" do
+    it "should give each player five cards" do
+      players.each do |player|
+        expect(player).to receive(:add_cards) do |cards|
+          expect(cards.length).to eq(5)
+        end
+      end
+      subject.deal_cards
+    end
+  end
+
+  describe '#play_draw' do
+    it "should give a player the number of cards they discard" do
+      expect(players.first).to receive(:add_cards) do |array|
+        expect(array.length).to eq(2)
+      end
+      expect(players.last).to receive(:add_cards)
+      subject.play_draw
+    end
+  end
+
+  describe "#take_bets" do
+    describe "taking bets from all players" do
+      before do
+        players.each do |player|
+         expect(player).to receive(:place_bet).and_return(1)
+        end
+      end
+      it "should ask all players to place bets" do
+        subject.take_bets
+      end
+
+      it "should increase its pot by the amount all players return" do
+        expect { subject.take_bets }.to change { subject.pot }.by(2)
+      end
+    end
+
+    it "should pass successive players the current highest bet" do
+      expect(players.first).to receive(:place_bet).with(0).and_return(2)
+      expect(players.last).to receive(:place_bet).with(2).and_return(2)
+      subject.take_bets
+    end
+
+    it "should remove a player who folds from the game" do
+      expect(players.first).to receive(:place_bet).and_return(-1)
+      expect(players.last).to receive(:place_bet).and_return(1)
+      subject.take_bets
+      expect(subject.players).to eq([players.last])
+    end
+  end
+
+end
