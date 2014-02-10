c94c2d57e11eb45e9cc63868e71ce05350bccfd4
Will Hastings <will@objectdotcreate.net>
2014-02-09 18:36:50 -0800

    Enable taking multiple moves per turn.

--- lib/checkers/human_player.rb
+++ lib/checkers/human_player.rb
@@ -5,14 +5,22 @@ module Checkers
     def play_turn
       puts "Piece to move (x,y):"
       start_pos = get_input
-      puts "Position to move to (x,y):"
-      end_pos = get_input
-      [start_pos, end_pos]
+      moves = []
+      loop do
+        puts "Position to move to next (x,y)"
+        puts "Or leave empty to stop selecting moves:"
+        next_move = get_input
+        break if next_move.nil?
+        moves << next_move
+      end
+      raise ArgumentError, "No move selected." if moves.empty?
+      [start_pos, moves]
     end
 
     def get_input
       input = STDIN.gets.chomp
       input = input.scan(/\d/)
+      return nil if input.empty?
       unless input.count == 2 && input.all? { |num| (0..7).cover?(num.to_i) }
         raise ArgumentError, "Position entered was invalid"
       end
