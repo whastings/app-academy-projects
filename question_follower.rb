39b56e2c9e7f34812a30f7a1321595ad1c5fdda1
Will Hastings <will@objectdotcreate.net>
2014-02-11 13:31:23 -0800

    Add QuestionFollower class.

---
+++ question_follower.rb
@@ -0,0 +1,69 @@
+require_relative 'questions_database'
+require_relative 'user'
+require_relative 'questions'
+
+class QuestionFollower
+  attr_reader :id
+  attr_accessor :user_id, :question_id
+
+  def self.all
+    results = QuestionsDatabase.instance
+      .execute('SELECT * FROM question_followers;')
+    results.map { |result| self.new(result) }
+  end
+
+  def self.find_by_id(id)
+    find_following = <<-SQL
+      SELECT
+        *
+      FROM
+        question_followers
+      WHERE
+        id = ?
+    SQL
+
+    following_data = QuestionsDatabase.instance.execute(find_following, id)
+    self.new(following_data)
+  end
+
+  def self.followers_for_question_id(question_id)
+    find_followers = <<-SQL
+      SELECT
+        *
+      FROM
+        question_followers
+      INNER JOIN
+        users
+        ON
+          question_followers.user_id = users.id
+      WHERE
+        question_followers.question_id = ?
+    SQL
+    followers = QuestionsDatabase.instance.execute(find_followers, question_id)
+    followers.map { |follower| User.new(follower) }
+  end
+
+  def self.followed_questions_for_user_id(user_id)
+    find_questions = <<-SQL
+      SELECT
+        *
+      FROM
+        question_followers
+      INNER JOIN
+        questions
+      ON
+        question_followers.question_id = questions.id
+      WHERE
+        question_followers.user_id = ?
+    SQL
+    questions = QuestionsDatabase.instance.execute(find_questions, user_id)
+    questions.map { |question| Question.new(question) }
+  end
+
+  def initialize(options = {})
+    @id, @user_id, @question_id =
+      options.values_at('id', 'user_id', 'question_id')
+    @db = QuestionsDatabase.instance
+  end
+
+end
