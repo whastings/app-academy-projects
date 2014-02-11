0eff9b2c31e6eac14d65dca7ec24103b55caaa93
Will Hastings <will@objectdotcreate.net>
2014-02-11 11:31:10 -0800

    Create user class.

---
+++ user.rb
@@ -0,0 +1,54 @@
+require_relative "questions_database"
+
+class User
+  attr_reader :id
+  attr_accessor :first_name, :last_name
+
+  def self.all
+    results = QuestionsDatabase.instance.execute('SELECT * FROM users;')
+    results.map { |result| self.new(result) }
+  end
+
+  def self.find_by_id(id)
+    find_user = <<-SQL
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = ?
+    SQL
+
+    user_data = QuestionsDatabase.instance.execute(find_user, id)
+    self.new(user_data)
+  end
+
+  def self.find_by_name(first_name, last_name)
+    find_user = <<-SQL
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        fname = ?
+        AND lname = ?
+    SQL
+
+    user_data = QuestionsDatabase.instance.execute(find_user, first_name, last_name)
+    self.new(user_data)
+  end
+
+  def initialize(options = {})
+    @id, @first_name, @last_name = options.values_at('id', 'fname', 'lname')
+    @db = QuestionsDatabase.instance
+  end
+
+  def authored_questions
+    Question.find_by_author_id(@id)
+  end
+
+  def authored_replies
+    Reply.find_by_user_id(@id)
+  end
+
+end
