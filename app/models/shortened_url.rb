2094ca6b16361d169e0723d7e7e537ee61a7a9cd
Will Hastings <will@objectdotcreate.net>
2014-02-12 15:10:03 -0800

    Add short_url class

---
+++ app/models/shortened_url.rb
@@ -0,0 +1,5 @@
+class ShortenedUrl < ActiveRecord::Base
+  validates :short_url, presence: true, uniqueness: true
+  validates :long_url, presence: true, uniqueness: true
+  validates :submitter_id, presence: true
+end
