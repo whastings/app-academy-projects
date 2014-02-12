7cbdff53512ee271ac33038dd019ab3ac06abe57
Will Hastings <will@objectdotcreate.net>
2014-02-12 14:50:01 -0800

    Add Postgresql as database. Set up user table.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortener::Application.config.secret_key_base = '939bb5efe126029b918f5682217c99dbecd14648b3a6a54ba8353de792e38597674a40b2c7c32426b3914517a76c0027c7403a5f96899a870058669c99aaf470'
---
+++ db/migrate/20140212224002_create_users.rb
@@ -0,0 +1,8 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+    end
+    add_index :users, :email, unique: true
+  end
+end
