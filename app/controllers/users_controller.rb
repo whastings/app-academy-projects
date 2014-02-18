0bac628aa5a29d9f946b4a79cb89418d65596969
Will Hastings <will@objectdotcreate.net>
2014-02-18 14:18:45 -0800

    Set up Contacts controller and API.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,41 +6,42 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render :json => user
+    @user = User.new(user_params)
+    if @user.save
+      render :json => @user
     else
-      render :json => user.errors.full_messages,
+      render :json => @user.errors.full_messages,
         :status => :unprocessable_entity
     end
   end
 
   def show
-    user = User.find_by_id(params[:id])
-    render :json => user
+    @user = User.find_by_id(params[:id])
+    render :json => @user
   end
 
   def update
-    user = User.find_by_id(params[:id])
-    if user && user.update_attributes(user_params)
-      render :json => user
+    @user = User.find_by_id(params[:id])
+    if @user && @user.update_attributes(user_params)
+      render :json => @user
     else
-      render :json => user.errors.full_messages,
+      render :json => @user.errors.full_messages,
         :status => :unprocessable_entity
     end
   end
 
   def destroy
-    user = User.find_by_id(params[:id])
-    if user && user.destroy
-      render :json => {}
+    @user = User.find_by_id(params[:id])
+    if @user && @user.destroy
+      render :json => @user
     else
       head status: 404
     end
   end
 
   private
+
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:username, :name, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 ContactsAPI::Application.routes.draw do
-  # resources :users
+  resources :users, only: [:create, :destroy, :index, :show, :update] do
+    resources :contacts, only: [:index]
+  end
+
+  resources :contacts, only: [:create, :destroy, :show, :update]
+
   # Doing it the hard way!
-  get '/users', to: 'users#index'
-  get '/users/new', to: 'users#new', as: 'new_user'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
-  get '/users/:id', to: 'users#show', as: 'user'
-  post '/users', to: 'users#create'
-  patch '/users/:id', to: 'users#update'
-  put '/users/:id', to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
+  # get '/users', to: 'users#index'
+  # get '/users/new', to: 'users#new', as: 'new_user'
+  # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  # get '/users/:id', to: 'users#show', as: 'user'
+  # post '/users', to: 'users#create'
+  # patch '/users/:id', to: 'users#update'
+  # put '/users/:id', to: 'users#update'
+  # delete '/users/:id', to: 'users#destroy'
 end
