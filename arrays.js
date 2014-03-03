62c0530e1f7413e339af2ca01a31408d61366b42
Will Hastings <will@objectdotcreate.net>
2014-03-03 11:09:16 -0800

    Solve array and enumerable JS problems.

---
+++ arrays.js
@@ -0,0 +1,54 @@
+;(function(){
+
+  Array.prototype.dups = function() {
+
+    var dups = [];
+    for (var i = 0, len = this.length; i < len; i++ ) {
+      if (dups.indexOf(this[i]) === -1) {
+        dups.push(this[i]);
+      }
+    }
+    return dups;
+
+  };
+
+  Array.prototype.twoSum = function() {
+
+    var sums = [];
+    for (var i = 0, ilen = this.length-1; i < ilen; i++) {
+      for (var j = i+1, jlen = this.length; j < jlen; j++) {
+        if (this[i] + this[j] === 0) {
+          sums.push([i, j]);
+        }
+      }
+    }
+    return sums;
+
+  };
+
+  Array.prototype.transpose = function() {
+    var trans = [];
+    for (var i = 0, len = this[0].length; i < len; i++) {
+      trans.push([]);
+    }
+    for (var i = 0, iLen = this.length; i < iLen; i++) {
+      for (var j = 0, jLen = this[i].length; j < jLen; j++) {
+        trans[i].push(this[j][i]);
+      }
+    }
+    return trans;
+  };
+
+}());
+
+_l = console.log;
+
+// console.log([1, 2, 3, 2, 1].dups());
+// _l([-1, 0, 2, -2, 1].twoSum());
+
+rows = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8]
+];
+_l(rows.transpose());
---
+++ enumerable.js
@@ -0,0 +1,46 @@
+;(function() {
+  var _l = console.log;
+
+  var twoMultiples = function(arr) {
+    return arr.map(function(el) {
+      return el * 2;
+    });
+  };
+
+  // _l(twoMultiples([8, 2, 4, 5, 9]));
+
+  var myEach = function(arr, callback) {
+    if (arr.length === 0 ) { return; };
+    arr = arr.slice();
+    callback(arr.shift());
+    myEach(arr, callback);
+  };
+
+  // myEach([1, 2, 3, 4, 5], function(el) {
+  //   _l(el);
+  // });
+
+  var myMap = function(arr, callback) {
+    var ret = [];
+    myEach(arr, function(el){
+      ret.push(callback(el));
+    });
+    return ret;
+  };
+
+  // _l(myMap([1, 2, 3, 4, 5], function(el) {
+  //   return el*2;
+  // }));
+
+  var myInject = function(arr, accum, callback) {
+    myEach(arr, function(el) {
+      accum = callback(accum, el);
+    });
+    return accum;
+  };
+
+  _l(myInject([1, 2, 3, 4, 5], 0, function(accum, el) {
+    return accum + el;
+  }));
+
+}());
